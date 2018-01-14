f5dbc15fd0d5492c4253f3bf70e0f4d777539015
wtfauonabt <stanleyhahaha@gmail.com>
2018-01-14 13:16:55 +1300

    v_0_4 Update 1
    - Added more portfolio
    - Made changes to CSS button in Contact Form

--- src/339_a2/models/obj/account.php
+++ src/339_a2/models/obj/account.php
@@ -1,72 +1,72 @@
-<?php
-/***********************************
-159339 Assignment 2
---------------------------------------
-Name: Chi Fung Stanley Yeung
-ID: 15316357
---------------------------------------
-Purpose:
-Stores information of 1 account.
-
-Contains functions for obtaining account information and transaction
-calculations.
-
-Class from Assignment 1
-*************************************/
-
-class Account
-{
-    private $id;        # Account ID
-    private $bal;   # Account Balance
-
-    /********************
-    // Constructor
-    *********************/
-    public function __construct($id, $amount)
-    {
-        $this->id = intval($id);
-        $this->bal = floatval($amount);
-    }
-
-    /********************
-    // FUNCTIONS
-    *********************/
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    public function getBal()
-    {
-        return $this->bal;
-    }
-
-    public function depositWithdraw($type, $amount)
-    {
-
-        // Checks for correct transaction input.
-        if (!ctype_alpha($type) || strlen($type) != 1)
-            throw new Exception("Invalid Type input: " . $type);
-        if (!is_numeric($amount))
-            throw new Exception("Invalid Amount input: " . $amount);
-
-        // Checks if amount is negative
-        if ($amount < 0)
-            throw new Exception("Negative Transaction Amount");
-
-        // Identify deposit or withdraw. Case insensitve.
-        if ($type == 'D' || $type == 'd'){
-            $this->bal += $amount;
-
-        } else if($type == 'W' || $type == 'w'){
-            //  Checks that there is enough balance to withdraw
-            if ($this->bal < $amount)
-                throw new Exception("Insufficient Balance");
-
-            $this->bal -= $amount;
-
-        } else {
-            throw new Exception("Invalid Type");
-        }
-    }
-}
+<?php
+/***********************************
+159339 Assignment 2
+--------------------------------------
+Name: Chi Fung Stanley Yeung
+ID: 15316357
+--------------------------------------
+Purpose:
+Stores information of 1 account.
+
+Contains functions for obtaining account information and transaction
+calculations.
+
+Class from Assignment 1
+*************************************/
+
+class Account
+{
+    private $id;        # Account ID
+    private $bal;   # Account Balance
+
+    /********************
+    // Constructor
+    *********************/
+    public function __construct($id, $amount)
+    {
+        $this->id = intval($id);
+        $this->bal = floatval($amount);
+    }
+
+    /********************
+    // FUNCTIONS
+    *********************/
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getBal()
+    {
+        return $this->bal;
+    }
+
+    public function depositWithdraw($type, $amount)
+    {
+
+        // Checks for correct transaction input.
+        if (!ctype_alpha($type) || strlen($type) != 1)
+            throw new Exception("Invalid Type input: " . $type);
+        if (!is_numeric($amount))
+            throw new Exception("Invalid Amount input: " . $amount);
+
+        // Checks if amount is negative
+        if ($amount < 0)
+            throw new Exception("Negative Transaction Amount");
+
+        // Identify deposit or withdraw. Case insensitve.
+        if ($type == 'D' || $type == 'd'){
+            $this->bal += $amount;
+
+        } else if($type == 'W' || $type == 'w'){
+            //  Checks that there is enough balance to withdraw
+            if ($this->bal < $amount)
+                throw new Exception("Insufficient Balance");
+
+            $this->bal -= $amount;
+
+        } else {
+            throw new Exception("Invalid Type");
+        }
+    }
+}
--- src/339_a2/models/obj/user.php
+++ src/339_a2/models/obj/user.php
@@ -1,41 +1,41 @@
-<?php
-/***********************************
-159339 Assignment 2
---------------------------------------
-Name: Chi Fung Stanley Yeung
-ID: 15316357
---------------------------------------
-Purpose:
-Stores information of a user.
-*************************************/
-
-class User
-{
-    private $id;
-    private $name;
-
-    public function __construct()
-    {
-        $this->id = NULL;
-        $this->name = NULL;
-    }
-
-    public function getId()
-    {
-        return $this->id;
-    }
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-}
+<?php
+/***********************************
+159339 Assignment 2
+--------------------------------------
+Name: Chi Fung Stanley Yeung
+ID: 15316357
+--------------------------------------
+Purpose:
+Stores information of a user.
+*************************************/
+
+class User
+{
+    private $id;
+    private $name;
+
+    public function __construct()
+    {
+        $this->id = NULL;
+        $this->name = NULL;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
